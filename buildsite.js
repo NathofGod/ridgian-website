0cb316bec993920a3ea892a236606d905984312d
Nathan Thomas <oaonath@gmail.com>
2015-10-07 19:34:07 +0100

    added build site for metalsmith

---
+++ buildsite.js
@@ -0,0 +1,33 @@
+require('harmonize')();
+
+var Metalsmith = require('metalsmith'),
+  helpers = require('metalsmith-register-helpers'),
+  clean = require('metalsmith-clean'),
+  markdown = require('metalsmith-markdown'),
+  templates = require('metalsmith-templates'),
+  dataMarkdown = require('metalsmith-data-markdown'),
+  contentful = require('contentful-metalsmith');
+
+
+Metalsmith(__dirname)
+  .destination('./public/build')
+  .source('src')
+  .use(markdown())
+  .use(helpers({
+    directory: 'server/handlebars/helpers'
+  }))
+  .use(clean(true))
+   .use(contentful({
+    accessToken: '9c58df927542109a651d0193a7843ba16877d698b11896935565f3e336e8c20f'
+  }))
+  .use(templates({
+    engine: 'handlebars',
+    directory: 'templates'
+  }))
+  .use(dataMarkdown({
+    removeAttributeAfterwards: true
+  }))
+  .build(function (err) {
+    if (err) console.log(err);
+  });
+
