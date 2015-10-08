6e33e09129838d295aceab01f056f2a83456cec4
Nathan Thomas <oaonath@gmail.com>
2015-10-08 09:32:30 +0100

    Attempt to have correct packages

--- deploy.sh
+++ deploy.sh
@@ -110,10 +110,10 @@ fi
 selectNodeVersion
 
 # 3. Install npm packages
-echo Install node
+echo Install npm packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install
+  eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
